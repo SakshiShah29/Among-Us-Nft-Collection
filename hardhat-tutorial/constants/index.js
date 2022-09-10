585b742029ea2c062f5f592fb235f8c86879cdf9
SakshiShah29 <sakshi.shah299@gmail.com>
2022-09-10 12:25:15 +0530

    Deployed

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xBb1BECA0cDE66aF62Dbe4cfdE48cC2bFDd9921E9";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://among-us-nft-collection-git-main-sakshishah29.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
